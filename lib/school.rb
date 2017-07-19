68ed1eb2eb9ddf0243b827963c452502335e2d34
joshlacey <williamjoshualacey@gmail.com>
2017-07-19 16:12:19 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor
+  attr_reader :school
+
+
+
+
+
+  def initialize(school)
+    @school = school
+    @roster = Hash.new{|hash, key| hash[key] = []}
+  end
+
+  def add_student(name, grade)
+    #@roster[grade] ||= [] <<< use this if no @roster Hash.new{....etc....}
+    @roster[grade] << name
+    @grade = grade
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def roster
+    @roster
+  end
+
+  def sort
+    new_hash = Hash.new{|hash, key| hash[key] = []}
+    @roster.each do |key, value|
+      new_hash[key] = value.sort   ## value.sort! works if you want to changed the original's value.
+    end
+    new_hash
+  end
+
+end
+
+school = School.new("Flatiron")
+
+#binding.pry
+"k"
